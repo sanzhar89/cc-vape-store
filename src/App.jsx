ae37b4cb9b24ebe313acf5f515e229a1d1749c26
Doshik <sanzhar_022@mail.ru>
2023-06-23 03:51:41 +0600

    Adaptive layout, adding cart page

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,11 @@ function App() {
       <div className="wrapper">
         <Header />
         <div className="content">
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/cart" element={<Cart />}></Route>
-              <Route path="*" element={<NotFound />}></Route>
-            </Routes>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
         </div>
       </div>
     </div>
